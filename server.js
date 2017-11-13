4bf64fba9db79f51c1ce791ee8213e4f8109165f
Daniel <danieljab7@gmail.com>
2017-11-12 19:52:28 -0500

    Properly implemented relevant callbacks, linked server to clientside for scratch frontend

--- server.js
+++ server.js
@@ -3,124 +3,84 @@ var express = require('express');
 var bodyParser = require('body-parser'); //The bodyParser() module is used for parsing the request body, so you can read post data
 var fileUpload = require('express-fileUpload');
 var fs = require('fs');
-var request = require('request');
 var path = require('path');
 var server = express();
 var najax = require('najax');
 
 server.get("/", function(req, res){
-    res.sendFile(__dirname + "/client.html");
+  res.sendFile(__dirname + "/client.html");
+})
+
+server.get("/test", function(req, res) {
+  console.log("it worked");
+  res.send("test")
 })
 
 //Sends input URL to the command prompt
 server.use(bodyParser());
-server.post('/', function(req, res) {
-  //res.sendStatus(200);
-  console.log(req.body);
+server.get('/getRaceData', function(req, res) {
+    //res.sendStatus(200);
+    console.log(req.query);
 
-//Use of KAIROS API to upload image information to a JSON file
+  //Use of KAIROS API to upload image information to a JSON file
 
-//API ID and Key for usage of API
+  //API ID and Key for usage of API
   var headers = {
-  "app_id"          : "969bb654",
-  "app_key"         : "8fe61971bbb4b59f034db4ddf6528043"
-};
+    "app_id"          : "969bb654",
+    "app_key"         : "8fe61971bbb4b59f034db4ddf6528043"
+  };
 
-//Image url to detect
-var payload  = req.body;
-//Test image { "image" : "http://dreamicus.com/data/face/face-04.jpg" };
+  //Image url to detect
+  var payload  = req.body;
+  //Test image { "image" : "http://dreamicus.com/data/face/face-04.jpg" };
 
-var url = "http://api.kairos.com/detect";
+  var url = "http://api.kairos.com/detect";
 
-// make request 
-  najax(url, {
-    headers  : headers,
-    type: "POST",
-    data: JSON.stringify(payload),
-    dataType: "text"}, 
-  function(html){ //Gets the JSON info in a single variable 'html'
-              fs.appendFile('output.json', JSON.stringify(html, null), function (err) {
-                if (err) throw err;
-                console.log('The data to append was appended to file!');
-            });
-});
-//Executing readfile() function after 10 seconds so that the contents have been written to the JSON file
-setTimeout(function(){
-    readfile();
-    }, 10000);
-setTimeout(function(){
-    fs.writeFile('output.JSON', '', function(){console.log('CLEAR')})
-    }, 15000);
+  // make request 
+  var options = {
+      headers  : headers,
+      type: "POST",
+      data: JSON.stringify(req.query),
+      dataType: "text"
+    };
+
+  najax(url, options, function(response) { //Gets the JSON info in a single variable 'html'
+    formatKairosData(response, function(kairosObject) {
+      res.send(kairosObject);
+    });
+  });
 });
 
 //Read output.JSON file and display contents on console
-function readfile(){
-  fs.readFile('output.JSON', 'utf8', function(err, contents) {
-
+function formatKairosData(kairosResponse, callback){
       //Splitting JSON file containing human analytics by \"
-      var info_array = contents.split('\\"');
+      console.log(JSON.parse(kairosResponse).images[0].faces)
+
+      var face = JSON.parse(kairosResponse).images[0].faces[0];
 
       //Defining arrays to store race and correlation value
-      var race_array = [];
-      var value_array = [];
+      var raceData = {
+        asian: face.attributes.asian,
+        white: face.attributes.white,
+        hispanic: face.attributes.hispanic,
+        black: face.attributes.black,
+        other: face.attributes.other
+      }
 
-      //Looping through the info_array to find all races
-      //Appending race into race_array and correlation value into value_array with matching indices
-      for (var j in info_array){
-        if (info_array[j] === "asian"){
-          race_array.push(info_array[j]);
-          value_array.push(info_array[++j]);
-          }
-        else if (info_array[j] === "hispanic"){
-          race_array.push(info_array[j]);
-          value_array.push(info_array[++j]);
-        }
-        else if (info_array[j] === "other"){
-          race_array.push(info_array[j]);
-          value_array.push(info_array[++j]);
-        }
-        else if (info_array[j] === "black"){
-          race_array.push(info_array[j]);
-          value_array.push(info_array[++j]);
-        }
-        else if (info_array[j] === "white"){
-          race_array.push(info_array[j]);
-          value_array.push(info_array[++j]);
+      var requiredData = {
+        race: null,
+        max: 0
+      };
+
+      for (var race in raceData) {
+        if (requiredData.max < raceData[race]) {
+          requiredData.race = race;
+          requiredData.max = raceData[race];
         }
       }
 
-      //Cleaning correlation values and parsing to convert the string values to floats
-      for (var x in value_array){
-        value_array[x] = value_array[x].replace(':','');
-        value_array[x] = value_array[x].replace(',','');
-        value_array[x] = parseFloat(value_array[x]);
-      }
-
-      //Initializing variables to find the maximum correlation value and respective index
-      var max = 0;
-      var max_index;
-
-      //Looping through value_array and finding maximum correction value and matching index for race
-      for (var y in value_array){
-        if (value_array[y] > max){
-          max = value_array[y];
-          max_index = y;
-        }
-      }
-
-      /*
-      for (var k in race_array){
-        console.log(race_array[k]);
-      }
-      for (var l in value_array){
-        console.log(value_array[l]);
-      }
-      */
-
-      //Displaying the race with the highest correlation value
-      console.log(race_array[max_index] + ": " + max);
-
-  });
+      console.log(requiredData);
+      callback(requiredData);
  }
 
 /*
